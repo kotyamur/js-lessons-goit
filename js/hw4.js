38c573fcf50d1c40f3a1aa8540bdcf8408b1d992
Kateryna Kleshch <ketrins1993@gmail.com>
2022-07-15 23:18:40 +0300

    add some tasks

--- js/hw4.js
+++ js/hw4.js
@@ -285,3 +285,139 @@
 // const getTotalBalanceByGender = (users, gender) => {
 //   return users.filter(user => user.gender === gender).reduce((acc, user) => acc + user.balance, 0);
 // };
+
+/**
+ * Example 1 - Коллбек функции
+ * Напишите следующие функции:
+ * createProduct(obj, callback) - 
+ * принимает объект товара без id, а также колбек. 
+ * Функция создаёт обьект товара, 
+ * добавляя ему уникальный идентификатор в свойство id и 
+ * вызывает колбек передавая ему созданный обьект.
+ * 
+ * logProduct(product) - коллбек принимающий обьект продукта 
+ * и логирующий его в консоль
+ * logTotalPrice(product) - коллбек принимающий обьект продукта 
+ * и логирующий общую стоимость товара в консоль
+ */
+// const p = {
+//     name: 'iPhone',
+//     price: 1000,
+//     count: 10
+// }
+// const createProduct = (obj, callback) => {
+//     const product = {
+//       ...obj,
+//       id: Math.random(),
+//     };
+//     callback(product);
+// };
+// const logProduct = product => {
+//     console.log(product);
+// };
+// // const logTotalPrice = product => {
+// //   console.log(product.price * product.count);
+// // };
+// const logTotalPrice = ({price, count}) => {
+//   console.log(price * count);
+// };
+
+// createProduct(p, logProduct)
+// createProduct(p, logTotalPrice)
+
+/**
+ * Example 2
+ * Напишите функцию аналог метода forEach
+ * Он должен принимать массив и callback функцию 
+ * которая будет вызываться для каждого элемента массива
+ */
+// const users = [
+//     { name: 'Denis', age: 33 },
+//     { name: 'Dima', age: 46 }
+// ]
+// const forEach = (arr, callback) => {
+//     for (let i = 0; i < arr.length; i += 1) {
+//       callback(arr[i], i, arr);
+//     }
+// }
+
+// const forEachCallback = (element, index, array) => {
+//     console.log(element);
+//     console.log(index);
+//     console.log(array);
+// };
+// forEach(users, forEachCallback)
+
+/**
+ *  Example 3 - Коллбек функции
+ * Напишите функцию each(array, callback), 
+ * которая первым параметром ожидает массив, а вторым - функцию, 
+ * которая применится к каждому элементу массива. 
+ * Функция each должна вернуть новый массив, 
+ * элементами которого будут результаты вызова коллбека.
+*/
+// const users = [
+//   { name: 'Denis', age: 33 },
+//   { name: 'Dima', age: 46 },
+// ];
+
+// const each = (arr, callback) => {
+//     const newArr = [];
+//     for (let i = 0; i < arr.length; i += 1) {
+//         const callbackResult = callback(arr[i], i, arr);
+//         newArr.push(callbackResult);
+//     }
+//     return newArr;
+// }
+
+// const getName = (user) => {
+//     return user.name;
+// }
+
+// console.log(each(users, getName));
+
+/**
+ * Example 4 - Коллбек функции
+ * Добавьте объекту account методы 
+ * withdraw(amount, onSuccess, onError) и 
+ * deposit(amount, onSuccess, onError), 
+ * где первый параметр это сумма операции, а второй и третий - колбеки.
+ * Метод withdraw вызывает onError если amount больше 
+ * TRANSACTION_LIMIT или this.balance, 
+ * и onSuccess в противном случае.
+ * Метод deposit вызывает onError если amount больше 
+ * TRANSACTION_LIMIT или меньше либо равен нулю, 
+ * и onSuccess в противном случае.
+ */
+
+// const TRANSACTION_LIMIT = 1000;
+
+// const account = {
+//   username: 'Jacob',
+//   balance: 400,
+
+//   deposit(amount, onSuccess, onError) {
+//     if (amount > TRANSACTION_LIMIT || amount <= 0) {
+//         return onError('Amount error')
+//     }
+
+//     this.balance += amount
+
+//     onSuccess(this.balance)
+//   },
+
+//   withdraw(amount, onSuccess, onError) {
+//     if (amount > TRANSACTION_LIMIT || amount > this.balance) {
+//         return onError('Amount error');
+//     }
+
+//     this.balance -= amount
+//     onSuccess(this.balance);
+//   }
+// }
+
+// const onSuccess = (balance) => console.log('onSuccessHandler', balance)
+// const onError = (error) => console.error('onErrorHandler', error)
+
+// account.deposit(2000, onSuccess, onError)
+// account.deposit(1000, onSuccess, onError)
---
+++ js/hw5.js
@@ -0,0 +1,281 @@
+// 1/20
+
+// const pizzaPalace = {
+//   pizzas: ['Supercheese', 'Smoked', 'Four meats'],
+//   checkPizza(pizzaName) {
+//     return this.pizzas.includes(pizzaName);
+//   },
+//   order(pizzaName) {
+//     const isPizzaAvailable = this.checkPizza(pizzaName);
+
+//     if (!isPizzaAvailable) {
+//       return `Sorry, there is no pizza named «${pizzaName}»`;
+//     }
+
+//     return `Order accepted, preparing «${pizzaName}» pizza`;
+//   },
+// };
+
+// 2/20
+
+// const customer = {
+//   username: "Mango",
+//   balance: 24000,
+//   discount: 0.1,
+//   orders: ["Burger", "Pizza", "Salad"],
+//   getBalance() {
+//     return this.balance;
+//   },
+//   getDiscount() {
+//     return this.discount;
+//   },
+//   setDiscount(value) {
+//     this.discount = value;
+//   },
+//   getOrders() {
+//     return this.orders;
+//   },
+//   addOrder(cost, order) {
+//     this.balance -= cost - cost * this.discount;
+//     this.orders.push(order);
+//   },
+// };
+
+// customer.setDiscount(0.15);
+// console.log(customer.getDiscount()); // 0.15
+// customer.addOrder(5000, "Steak");
+// console.log(customer.getBalance()); // 19750
+// console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+
+// 3/20
+
+// const historyService = {
+//   orders: [
+//     { email: 'jacob@hotmail.com', dish: 'Burrito' },
+//     { email: 'solomon@topmail.net', dish: 'Burger' },
+//     { email: 'artemis@coldmail.net', dish: 'Pizza' },
+//     { email: 'solomon@topmail.net', dish: 'Apple pie' },
+//     { email: 'jacob@hotmail.com', dish: 'Taco' },
+//   ],
+//   getOrdersLog() {
+//     return this.orders.map(order => `email: ${order.email} dish: ${order.dish}`).join(' - ');
+//   },
+//   getEmails() {
+//     const emails = this.orders.map(order => order.email);
+//     const uniqueEmails = new Set(emails);
+//     return [...uniqueEmails];
+//   },
+//   getOrdersByEmail(email) {
+//     return this.orders.filter(order => order.email === email);
+//   },
+// };
+
+// 5/20
+
+// const ancestor = {
+//   name: 'Paul',
+//   age: 83,
+//   surname: 'Dawson',
+//   heritage: 'Irish',
+// };
+
+// const parent = Object.create(ancestor);
+// parent.name = 'Stacey';
+// parent.surname = 'Moore';
+// parent.age = 54;
+
+// const child = Object.create(parent);
+// child.name = 'Jason';
+// child.age = 27;
+
+// 9/20
+
+// class Car {
+//   constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   getPrice() {
+//     return this.price;
+//   }
+//   changePrice(newPrice) {
+//     this.price = newPrice;
+//   }
+// }
+
+// 10/20
+
+// class Storage {
+//     constructor (items) {
+//         this.items = items;
+//     }
+//     getItems() {
+//         return this.items;
+//     }
+//     addItem(newItem) {
+//         this.items.push(newItem);
+//     }
+//     removeItem(itemToRemove) {
+//         this.items = this.items.filter(item => item !== itemToRemove);
+//     }
+// }
+
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+// 11/20
+
+// class StringBuilder {
+//     constructor (initialValue) {
+//         this.value = initialValue;
+//     }
+//     getValue() {
+//         return this.value;
+//     }
+//     padEnd(str) {
+//         this.value = this.value + str;
+//     }
+//     padStart(str) {
+//         this.value = str + this.value;
+//     }
+//     padBoth(str) {
+//         this.padStart(str);
+//         this.padEnd(str);
+//     }
+// }
+
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
+
+// 12/20
+
+// class Car {
+//   model;
+//   price;
+//   #brand;
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   getBrand() {
+//     return this.#brand;
+//   }
+//   changeBrand(newBrand) {
+//     this.#brand = newBrand;
+//   }
+// }
+
+// 16/20
+
+// class Car {
+//   #price;
+//   static MAX_PRICE = 50000;
+
+//   constructor({ price }) {
+//     this.#price = price;
+//   }
+
+//   get price() {
+//     return this.#price;
+//   }
+
+//   set price(newPrice) {
+//     if (newPrice > Car.MAX_PRICE) {
+//       return;
+//     }
+//     this.#price = newPrice;
+//   }
+// }
+
+// const audi = new Car({ price: 35000 });
+// console.log(audi.price); // 35000
+
+// audi.price = 49000;
+// console.log(audi.price); // 49000
+
+// audi.price = 51000;
+// console.log(audi.price); // 49000
+
+// 17/20
+
+// class Car {
+//   static #MAX_PRICE = 50000;
+//   static checkPrice(price) {
+//     if (price > Car.#MAX_PRICE) {
+//       return "Error! Price exceeds the maximum";
+//     }
+//     return "Success! Price is within acceptable limits";
+//   }
+//   constructor({ price }) {
+//     this.price = price;
+//   }
+// }
+
+// const audi = new Car({ price: 36000 });
+// const bmw = new Car({ price: 64000 });
+
+// console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+// console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+
+// 19/20
+
+// class User {
+//   email;
+
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+// class Admin extends User {
+
+//   static AccessLevel = {
+//     BASIC: "basic",
+//     SUPERUSER: "superuser",
+//   };
+
+//   constructor({ email, accessLevel }) {
+//     super(email);
+//     this.accessLevel = accessLevel;
+//     this.blacklistedEmails = [];
+//   }
+
+//   blacklist(email) {
+//     this.blacklistedEmails.push(email);
+//   }
+//   isBlacklisted(email) {
+//     return this.blacklistedEmails.includes(email);
+//   }
+// }
+
+// const mango = new Admin({
+//   email: "mango@mail.com",
+//   accessLevel: Admin.AccessLevel.SUPERUSER,
+// });
+
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.accessLevel); // "superuser"
+
+// mango.blacklist("poly@mail.com");
+// console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+// console.log(mango.isBlacklisted("mango@mail.com")); // false
+// console.log(mango.isBlacklisted("poly@mail.com")); // true
